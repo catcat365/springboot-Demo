3c7386c826c4cc3130af7975963f076249a6d712
catcat <18276610+catcat365@users.noreply.github.com>
2026-07-09 22:30:37 +0800

    多环境配置：配置 dev、test、prod 三套配置文件，一键切换数据库连接。

---
+++ 其他/springboot-db260709.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Ultimate v9.62 
+MySQL - 5.7.38 : Database - springboot_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `email` varchar(255) DEFAULT NULL COMMENT '用户邮箱',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`email`) values (2,'李四','lisi@example.com'),(3,'王五','wangwu@example.com'),(4,'李四','lisi@example.com'),(6,'王五','wangwu@example.com'),(7,'修改4445554','4444@qq.com'),(8,'李四','lisi@example.com'),(9,'李四6','lisi@example.com'),(10,'李四','lisi@example.com'),(11,'调试','调试@example.com');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
